bd75bc9d39e726dedc650632a9e50e7764ca2ede
sofas <olgaverakso1@gmail.com>
2026-07-08 22:13:05 +0500

    Fix: add function calls

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-<<<<<<< HEAD
-﻿print("hello")
+﻿print("hello everyone")
 
 def greetWhite():
     print("hello white")
-=======
-﻿print("hello everyone")
->>>>>>> 89a1bc1 (Change greeting to hello everyone)
 
 def greetBlack():
     print("hello black")
+
+# Вызовы функций
+greetWhite()
+greetBlack()
