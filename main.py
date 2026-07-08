3c125731af61695d4706904a397e91a58d7ec898
sofas <olgaverakso1@gmail.com>
2026-07-08 22:05:56 +0500

    Add greetBlack function

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
+<<<<<<< HEAD
 ﻿print("hello")
 
 def greetWhite():
     print("hello white")
+=======
+﻿print("hello everyone")
+>>>>>>> 89a1bc1 (Change greeting to hello everyone)
+
+def greetBlack():
+    print("hello black")
